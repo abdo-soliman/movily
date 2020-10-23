7fa17ade1b2e53fee184d83780d8af0ef2ece160
abdo-soliman <abdelrahman.soliman97@eng-st.cu.edu.eg>
2020-10-23 22:57:35 +0200

    added env example and footer component

---
+++ movily/src/components/Footer.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { Row, Col, Image } from "react-bootstrap";
+
+import "../styles/Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <Row className="footer-image-row">
+                <Col xs={0} md={3}></Col>
+                <Col xs={12} md={6}>
+                    <div className="footer-image-container">
+                        <Image src={require("../assets/the_moviedb_logo.svg")} className="footer-image" />
+                    </div>
+                </Col>
+                <Col xs={0} md={3}></Col>
+            </Row>
+            <Row className="footer-text-row">
+                <Col xs={1}></Col>
+                <Col xs={10}>
+                    <p className="footer-text">This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
+                </Col>
+                <Col xs={1}></Col>
+            </Row>
+        </footer>
+    )
+}
+
+export default Footer;
